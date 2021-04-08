8f74a259b1b210fae91d55b360e5e65d2dde17f3
Ariel Molho <arielmolho@gmail.com>
2021-04-08 20:28:30 -0300

    eliminando código y archivos innecesarios

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './Portyleria-Logo.jpg';
-//<img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Free_logo.svg" width="30" height="30" alt="Portyleria" loading="lazy"/>
-//3-ReactJS\portyleria-molho\src\components\roughLogo.png
-//C:\Users\Ari\Documents\ariel\1-Personal\CoderHouse\3-ReactJS\portyleria-molho\src\components\roughLogo.png
+
 function NavBar () {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="#">
-                <img src={logo} width="50" height="50" alt="Portyleria" loading="lazy"/>
+                <img src={logo} width="100" height="100" alt="Portyleria" loading="lazy"/>
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
