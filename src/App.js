73a2e43fb052199684b11a477446a80f9d8e9d2b
Ariel Molho <arielmolho@gmail.com>
2021-04-10 15:18:11 -0300

    ajustes desafío clase4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './containers/ItemListContainer/ItemListContainer';
 
-function App() {
-  return (
-    <div className="nav-div">
-      <NavBar />
-      <ItemListContainer />
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="nav-div">
+        <NavBar />
+        <ItemListContainer greeting="Bienvenid@!" name="PortyBox"/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/containers/ItemListContainer/ItemListContainer.js
+++ src/containers/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './ItemListContainer.css';
 
-export default class ItemListContainer extends React.Component{
-    render(){
-        return(
-            <div className="header">
-                <p>Tentate con nuestros <a className="header-portybox" href="#">PortyBox</a>! Ideales para compartir al aire libre.</p>
-            </div>
-        )
-    }
+export default function ItemListContainer(props) {
+    return(
+        <div className="header">
+            <p>{props.greeting}</p>
+            <p>Tentate con nuestros <a className="header-portybox" href="#">{props.name}</a>! Ideales para compartir al aire libre.</p>
+        </div>
+    )
 }
