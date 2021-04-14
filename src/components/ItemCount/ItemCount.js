edc3eab8910909d1816c046178cdf9bbb86474d5
Ariel Molho <arielmolho@gmail.com>
2021-04-14 20:38:48 -0300

    Desafío clase 5 Contador con botón

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './ItemCount.css';
+import alfajor from '../../assets/images/portyleria-alfajores.png';
+
+export default function ItemCount ({number, increment, decrement}) {
+    return(
+        <div className="card">
+            <img src={alfajor} className="card-img-top" alt="Alfajor"/>
+            <div className="card-body">
+                <h5 className="card-title">Alfajor</h5>
+                <p className="card-text">Alfajor de dulce de leche bañado en chocolate.</p>
+                <p className="card-text">Valor por unidad: $ 70.</p>
+            </div>
+            <div className="card-body d-flex justify-content-between">
+                {
+                    number > 0 ? <button onClick={decrement} className="btn btn-danger">-</button> : <button onClick={decrement} className="btn btn-danger" disabled>-</button>
+                }
+                <p className="visual align-self-center">{number}</p>
+                {
+                    number < 5 ? <button onClick={increment} className="btn btn-primary">+</button> : <button onClick={increment} className="btn btn-primary" disabled>+</button>
+                }
+                
+            </div>
+            <div className="card-body text-center">
+                {
+                    number < 5 ? <button onClick={increment} className="btn btn-success">Agregar al carrito</button> : <button onClick={increment} className="btn btn-success" disabled>Agregar al carrito</button>
+                }
+            </div>
+        </div>
+    )
+}
--- src/containers/ItemListContainer/ItemListContainer.js
+++ src/containers/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ItemListContainer.css';
+import ItemCount from '../../components/ItemCount/ItemCount';
 
-export default function ItemListContainer(props) {
+export default function ItemListContainer({name, greeting}) {
+    const [number, setNumber] = useState(1);
+
+    function onIncrement(){
+        setNumber(number + 1)
+    }
+
+    function onDecrement(){
+        setNumber(number - 1)
+    }
     return(
-        <div className="header">
-            <p>{props.greeting}</p>
-            <p>Tentate con nuestros <a className="header-portybox" href="#">{props.name}</a>! Ideales para compartir al aire libre.</p>
+        <div>
+            <div className="header">
+                <p className="header-text">{greeting}</p>
+                <p className="header-text">Tentate con nuestros <a className="header-portybox" href="#">{name}</a>! Ideales para compartir al aire libre.</p>
+            </div>
+            <div className="item-card">
+                <ItemCount number={number} increment={onIncrement} decrement={onDecrement}/>
+            </div>
         </div>
     )
 }
