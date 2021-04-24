d06c384ff5ac3f50758203fc23f4d014a1d4e25c
Ariel Molho <arielmolho@gmail.com>
2021-04-24 16:44:01 -0300

    avances primer entrega proyecto final

--- src/containers/PortyBoxPage/PortyBoxPageContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ItemDetailContainer from '../ItemDetailContainer/ItemDetailContainer';
-
-export default function PortyBoxPageContainer() {
-    return(
-        <ItemDetailContainer />
-    )
-}
