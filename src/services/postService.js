446da6b6b689cdc9757c0fd94ceff8b621ecfc6b
Ariel Molho <arielmolho@gmail.com>
2021-05-08 12:57:34 -0300

    borrando notas y código obsoleto

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,4 +1,4 @@
-import db from '../firebase'; //detecta por defecto que el archivo a usar es index.js
+import db from '../firebase';
 
 const itemCollection = db.collection("productos");
 
@@ -16,37 +16,3 @@ export function getPostById(idPost) {
       return snapshot.docs.map(doc => doc.data())
     })
 }
-
-/////////////////////////
-// LLAMADOS A FAKE API //
-/////////////////////////
-
-// function getPosts() {
-//   return new Promise((resolve, reject) => {
-//     fetch(`http://my-json-server.typicode.com/ArielMolho/mockjson/products`)
-//       .then(res => res.json())
-//       .then(data => resolve(data))
-//       .catch(err => reject(err))
-//   })
-// }
-
-// function getPostById(idPost) {
-//   return new Promise((resolve, reject) => {
-//     fetch(`http://my-json-server.typicode.com/ArielMolho/mockjson/products/${idPost}`)
-//       .then(res => res.json())
-//       .then(data => resolve(data))
-//       .catch(err => reject(err))
-//   })
-// }
-
-// module.exports = {
-//   getPostById,
-//   getPosts,
-// }
-
-/*
->> Consigna: conecta tu nueva ItemCollection de Google Firestore a tu ItemListContainer y ItemDetailContainer.
-Si navegas a /item/:id, debe ocurrir una consulta de (1) documento. 
-
-https://docs.google.com/presentation/d/1ncUKwUXDSCtunXEBCm4Wl8wC9k_-mu-XwatKoNJH7V4/edit#slide=id.gb19a8efb71_1_1297
-*/
